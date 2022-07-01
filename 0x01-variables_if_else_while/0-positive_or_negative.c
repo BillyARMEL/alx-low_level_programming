1062e5d6b46292ab2ee26b2da4e9c73059fc7666
BillyARMEL <billyessaga@yahoo.fr>
2022-07-01 16:18:55 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,12 @@ if (n > 0)
 }
 else if (n < 0)
 {
-printf("%d is negative\n", n);
+  printf("%d is negative\n", n);
+ }
 else
 {
-printf("%d is negative\n", n);
+  printf("%d is negative\n", n);
 }
-return (0);
+ return (0);
 }
 
