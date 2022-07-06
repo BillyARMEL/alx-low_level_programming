38dfda32e6eb6a5e997c03b8cb33d0cdcc6ed31f
BillyARMEL <billyessaga@yahoo.fr>
2022-07-05 17:32:52 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,43 +3,26 @@
 
 
 /**
-
- * print_alphabet_x10 - prints alphabet 10 times
-
- * Return:void
-
- */
-
+* print_alphabet_x10 - prints alphabet 10 times
+* Return:void
+*/
 void print_alphabet_x10(void)
-  
-{
-  
-  char c;
-  
-  int i = 0;
-  
 
+{
+
+char c;
   
-  while (i < 10)
-    
-    {
-      
-      c = 'a';
-      
-      while (c <= 'z')
-	
-	{
-	  
-	  _putchar(c);
-	  
-	  c++;
-	  
-	}
-      
-      _putchar('\n');
-      
-      i++;
-      
-    }
-  
+int i = 0;
+
+while (i < 10)
+{
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+i++;
+}
 }
