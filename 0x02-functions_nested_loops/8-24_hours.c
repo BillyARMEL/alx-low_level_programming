153d0aa7c18a1496d7235c09eda8ee01fc61bd4c
BillyARMEL <billyessaga@yahoo.fr>
2022-07-05 18:23:42 -0700

    8. I_m federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,10 +14,10 @@ for (b = 0; b <= 59; b++)
 {
 _putchar (a / 10 + '0');
 _putchar (a % 10 + '0');
-_putchar (':');	  
+_putchar (':');
 _putchar (b / 10 + '0');
-_putchar (b % 10 + '0');	  
+_putchar (b % 10 + '0');
 _putchar ('\n');
 }
-} 
+}
 }
