0bfd3a7ad89d6b242e02d223969aa5ea6700bed4
BillyARMEL <billyessaga@yahoo.fr>
2022-07-14 15:26:25 +0000

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,22 +3,22 @@
 
 
 /**
- * *_strcat - function that concatenates two strings.
- * *dest :  first parameter of the function (pointer to the first string)
- * *src :  second parameter of the function (pointer to the second string)
- * return : function return a pointer to the resulting string des
- */
+* *_strcat - function that concatenates two strings.
+* *dest :  first parameter of the function (pointer to the first string)
+* *src :  second parameter of the function (pointer to the second string)
+* return : function return a pointer to the resulting string des
+*/
 
 char *_strcat(char *dest, char *src)
 {
-  for( ; *dest != '\0'; dest++)
-    {
-      _putchar(*dest);
-    }
-  for(; *src != '\0'; src++)
-    {
-      _putchar(*src);
-    }
-  _putchar('\0');
-  return (dest);
+for( ; *dest != '\0'; dest++)
+{
+_putchar(*dest);
+}
+for(; *src != '\0'; src++)
+{
+_putchar(*src);
+}
+_putchar('\0');
+return (dest);
 }
