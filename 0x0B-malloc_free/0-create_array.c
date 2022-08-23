ac24b7f0961999075dab487ef691a64a64c9994c
BillyARMEL <106820432+BillyARMEL@users.noreply.github.com>
2022-08-23 18:27:46 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,19 +13,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-unsigned int i;
-char *iArray;
-if (size == 0)
-return (NULL);
-/* returns a pointer to the allocated memory */
-iArray = malloc(size * sizeof(char));
-if (iArray == NULL)
-return (NULL);
-i = 0;
-while (i < size)
-{
-iArray[i] = c;
-i++;
-}
-return (iArray);
+  unsigned int i;
+  char *iArray;
+  if (size == 0)
+    return (NULL);
+  /* returns a pointer to the allocated memory */
+  iArray = malloc(size * sizeof(char));
+  if (iArray == NULL)
+    return (NULL);
+  i = 0;
+  while (i < size)
+  {
+    iArray[i] = c;
+    i++;
+  }
+  return (iArray);
 }
